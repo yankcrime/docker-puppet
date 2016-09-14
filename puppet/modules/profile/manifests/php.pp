6f0bfec9853a9f1a357fc5bdd0bc858a1b407921
Nick Jones <nick@dischord.org>
2016-09-14 11:21:42 +0100

    WIP downgrade to PHP5

--- puppet/modules/profile/manifests/php.pp
+++ puppet/modules/profile/manifests/php.pp
@@ -2,32 +2,35 @@
 #
 class profile::php {
 
+  include ::apt
   include ::php::fpm::daemon
 
   file { '/run/php':
     ensure => 'directory',
-	}
+  }
 
+  # we need php5
+  apt::ppa { 'ppa:ondrej/php': }->
   php::fpm::conf { 'www':
-    listen                    => '/run/php/php7.0-fpm.sock',
+    package_name              => 'php5.6-fpm',
+    listen                    => '/run/php/php-fpm.sock',
     listen_owner              => 'www-data',
     listen_group              => 'www-data',
     user                      => 'www-data',
     security_limit_extensions => '.php',
     require                   => [ Package['nginx'], File['/run/php'] ],
+  }->
+  file_line { 'php-fpm.conf':
+    path  => '/etc/php5.6/fpm/php-fpm.conf',
+    line  => 'daemonize = no',
+    match => 'daemonize = yes',
   }
 
   php::module { ['mcrypt', 'mysql', 'curl', 'gd', 'intl', 'tidy', 'json']: }
 
-  file_line { 'php-fpm.conf':
-    path    => '/etc/php/7.0/fpm/php-fpm.conf',
-    line    => 'daemonize = no',
-    match   => 'daemonize = yes',
-    require => Package['php-fpm'],
-  }
 
   supervisord::program { 'php':
-    command => '/usr/sbin/php-fpm7.0',
+    command => '/usr/sbin/php-fpm5.6',
   }
 
 }
