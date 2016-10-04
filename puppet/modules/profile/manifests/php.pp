bd52fad105c7cc3c3a6a7543bedad0e59597dbaa
Nick Jones <nick@dischord.org>
2016-10-04 11:51:53 +0100

    Install PHP5 on Xenial instead of PHP7
    Mint doesn't support PHP7 and so we need to install PHP5 instead.

--- puppet/modules/profile/manifests/php.pp
+++ puppet/modules/profile/manifests/php.pp
@@ -9,8 +9,6 @@ class profile::php {
     ensure => 'directory',
   }
 
-  # we need php5
-  apt::ppa { 'ppa:ondrej/php': }->
   php::fpm::conf { 'www':
     package_name              => 'php5.6-fpm',
     listen                    => '/run/php/php-fpm.sock',
@@ -21,13 +19,12 @@ class profile::php {
     require                   => [ Package['nginx'], File['/run/php'] ],
   }->
   file_line { 'php-fpm.conf':
-    path  => '/etc/php5.6/fpm/php-fpm.conf',
+    path  => '/etc/php/5.6/fpm/php-fpm.conf',
     line  => 'daemonize = no',
     match => 'daemonize = yes',
   }
 
   php::module { ['mcrypt', 'mysql', 'curl', 'gd', 'intl', 'tidy', 'json']: }
-
 
   supervisord::program { 'php':
     command => '/usr/sbin/php-fpm5.6',
