7832a515aa79f5d53119fe533d4cb1a78f9f0f3c
Nick Jones <nick@dischord.org>
2017-03-30 08:25:32 +0000

    [Postfix] Use the right stdlib function

--- puppet/modules/profile/manifests/postfix.pp
+++ puppet/modules/profile/manifests/postfix.pp
@@ -2,7 +2,7 @@ class profile::postfix {
   include ::postfix
 
   postfix::config {
-    'relay_domains':      value => hiera_array(relay_domains);
+    'relay_domains':      value => join(hiera(relay_domains), " ");
     'virtual_alias_maps': value => 'hash:/etc/postfix/virtual';
     'myhostname':         value => 'antelope.dischord.org';
   }
@@ -10,7 +10,6 @@ class profile::postfix {
   postfix::hash { '/etc/postfix/virtual':
     ensure => present,
   }
-
 
   create_resources(postfix::virtual, hiera(mail_aliases))
 
